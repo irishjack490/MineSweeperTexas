2095a37c14d6785e28351cba4ed18a8464fbe6c7
CosmicGate <jackie.sechrist.3@gmail.com>
2023-11-29 11:44:30 -0600

    update pseudocode

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
- //DOM style grid property 
+ //DOM style grid property, size of grid: 4 rows, 4 columns, total of 16 same size squares
 
- //Manipulate DOM to render game upon loading browser 
+ //Generate 5 mines in random squares, generate numbers. 
+ //These must be non-visible until player clicks on the squares
 
- //Create Math function for computer to set 5 mines randomly with numbers on the table 
+ //Render game upon loading browser
 
+ //Listen for player's click 
 
- //Create an event listener to capture click of the player
-
- //Player will click on a square, a mine or a number will appear, if the square contains a mine the game will be over
- //If the square displays a number, player will have the change to click on another square
+ //If  player uncovers a mine the game will be over
+ //If player uncovers a number, player will have the chance to click on another square
  
- //set timer of 5 minutes to allow the player to complete the game, if mine is hit. Timer will stop
+ //set timer of 5 minutes to allow the player to complete the game, if mine is hit this, timer will stop 
 
- //Function to determine if player hit a mine or has another chance
-
- //Display the numbers that give the hint that a mine is close by (we could use 1 and 2 since the grid is 4x4)
-
- //Function to end game if player lost
+ //End game if player hits a mine
 
  //Give player the option to play again or quit 
